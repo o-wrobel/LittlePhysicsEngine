0e7391172f6367a2b1b0b5098d72d0e49047e59a
Oskar Wróbel <o.wrobel2025@protonmail.com>
2026-05-13 17:15:00 +0200

    Added simple_vectors imports in build.zig

--- build.zig
+++ build.zig
@@ -4,7 +4,14 @@ pub fn build(b: *std.Build) void {
 	const target = b.standardTargetOptions(.{});
 	const optimize = b.standardOptimizeOption(.{});
 
-	_ = b.addModule(
+	const vec_dep = b.dependency("simple_vectors", .{
+		.target = target,
+		.optimize = optimize,
+	});
+	const vec2_mod = vec_dep.module("Vector2");
+	const vec3_mod = vec_dep.module("Vector3");
+
+	const mod = b.addModule(
 		"Simulation",
 		.{
 			.root_source_file = b.path("src/Simulation.zig"),
@@ -12,4 +19,8 @@ pub fn build(b: *std.Build) void {
 			.optimize = optimize,
 		}
 	);
+
+	mod.addImport("Vector2", vec2_mod);
+	mod.addImport("Vector3", vec3_mod);
+
 }
